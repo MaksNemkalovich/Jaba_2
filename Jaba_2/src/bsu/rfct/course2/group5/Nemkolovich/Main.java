a6f3833f70c48b23740db67559e4d40badfb9039
Maks <Maks@DESKTOP-RE7O0M3>
2023-12-18 19:34:34 +0300

    Com

---
+++ Jaba_2/src/bsu/rfct/course2/group5/Nemkolovich/Main.java
@@ -0,0 +1,11 @@
+package bsu.rfct.course2.group5.Nemkolovich;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame(); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        frame.setVisible(true);
+    }
+}
